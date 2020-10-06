8655c16ca640f61de7fc9a7cbc772a2d366a25eb
Muhammad Fakhrulghazi bin Mohd Fouad <muhammadfakhrulghazibinmohdfouad@Muhammads-MBP.realtek>
2020-10-06 22:27:24 +0800

    fix

--- server.js
+++ server.js
@@ -25,26 +25,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users: [{
-        id: '123',
-        name: 'John',
-        email: 'john@gmail.com',
-        password: 'cookies',
-        entries: 0,
-        joined: new Date()
-    },
-    {
-        id: '124',
-        name: 'alya',
-        email: 'alya@gmail.com',
-        password: 'bananas',
-        entries: 0,
-        joined: new Date ()
-    }
-]
-}
-
 app.get('/',(req,res)=> {res.send('it is working!');
 })
 
